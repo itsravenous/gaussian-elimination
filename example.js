265ffcc9830f631a45ede46a7169dc7092c2c1b2
Tom Jenkins <tom@itsravenous.com>
2014-08-08 20:47:30 +0100

    Initial import. Algorith is a super-quick clone of the PHP version available at http://martin-thoma.com/solving-linear-equations-with-gaussian-elimination/#tocAnchor-1-5 (I had a couple difficulties with the JS version presented there)

---
+++ example.js
@@ -0,0 +1,41 @@
+var gauss = require('./gauss');
+
+/**
+ * Solves 4x = 8
+ */
+function test_one() {
+    $A = [[4]];
+    $x = [8];
+    $result = gauss($A, $x);
+    console.log($result);
+}
+
+/**
+ * Solves this system:
+ * x + y = 10
+ * 2x + y = 16
+ */
+function test_two () {
+    $A = [[1, 1], [2, 1]];
+    $x = [10, 16];
+    $result = gauss($A, $x);
+    console.log($result);
+
+}
+
+/**
+ * Solves this system:
+ * x + y = 10
+ * 2x + y = 16
+ * 3x + 2y = 24
+ */
+function test_three () {
+    $A = [[1, 1, 1], [2, 1, 2], [1, 2, 3]];
+    $x = [6, 10, 14];
+    $result = gauss($A, $x);
+    console.log($result);
+}
+
+test_one();
+test_two();
+test_three();
